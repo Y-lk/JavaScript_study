ba26a2eb3714dbf21b7ca854b583687cc89a2ed7
Yury <yuri.lasko@gmail.com>
2021-06-04 14:51:02 +0300

    My start study of JS.

---
+++ Lesson_2.4.js
@@ -0,0 +1,12 @@
+const obj = {
+  name: "John",
+  age: 25,
+  isMarried: false
+};
+
+//console.log(obj.name);
+console.log(obj["name"]);
+
+let arr = ['plum.png', 'orange.jpg',6 , 'apple.bmp', [ ], { }];
+
+console.log(arr[1]);
---
+++ Lesson_2.7.js
@@ -0,0 +1,23 @@
+"use strict";
+
+//alert("Hello!")
+
+// const result = confirm("Are you here?");
+// console.log(result);
+
+// const answer = prompt("Вам есть 18 лет?", "");
+// console.log(typeof(answer));
+
+// const answer = +prompt("Вам есть 18 лет?", "");
+// console.log(answer + 5);
+
+const answers = [];
+
+// answers[0] = prompt("How's your name?", "");
+// answers[1] = prompt("How's your surname?", "");
+// answers[2] = prompt("How's your age?", "");
+
+// document.write(answers);
+
+console.log(typeof(answers));
+console.log(typeof(null));
