82675e31ffcb9a544d725bde7c09533e03895974
Yury Lasko <yuri.lasko@gmail.com>
2021-06-22 18:55:34 +0300

    Lesson 2.22 was complete.

--- Lesson_2.22.js
+++ Lesson_2.22.js
@@ -19,35 +19,85 @@
 // console.log(copy1);
 // console.log(obj);
 
-function copy(mainObj){
-  let objCopy = {};
+// function copy(mainObj){
+//   let objCopy = {};
   
-  let key;
-  for(key in mainObj){
-    objCopy[key] = mainObj[key];
-  }
-  return objCopy;
-}
+//   let key;
+//   for(key in mainObj){
+//     objCopy[key] = mainObj[key];
+//   }
+//   return objCopy;
+// }
 
-const numbers = {
-  a: 2,
-  b: 5,
-  c: {
-    x: 7,
-    y: 4
-  }
-};
+// const numbers = {
+//   a: 2,
+//   b: 5,
+//   c: {
+//     x: 7,
+//     y: 4
+//   }
+// };
 
-const newNumbers = copy(numbers);
-newNumbers.c.x = 10;
+// const newNumbers = copy(numbers);
+// newNumbers.c.x = 10;
 
-newNumbers.a = 10;
-console.log(newNumbers);
-console.log(numbers);
+// newNumbers.a = 10;
+// console.log(newNumbers);
+// console.log(numbers);
 
 const add = {
   d: 17,
   e: 20
 };
 
-console.log(Object.assign(numbers, add));
+//console.log(Object.assign(numbers, add));
+
+const clone = Object.assign({}, add);
+
+clone.d = 20;
+console.log(add);
+console.log(clone);
+
+//
+
+const oldArray = ["a", "b", "c"];
+const newArray = oldArray.slice();
+
+newArray[1] = "abracadabra";
+console.log(newArray);
+console.log(oldArray);
+
+//
+
+const video = ["youtube", "vimeo", "rutube"],
+      blogs = ["wordpress", "livejornal", "blogger"],
+      internet = [...video, ...blogs, "vk", "facebook"];
+
+console.log(internet);
+
+function log (a, b, c){
+  console.log(a);
+  console.log(b);
+  console.log(c);
+}
+
+const num = [2, 5, 7];
+
+log (...num);
+
+//
+
+const array = ["a", "b"];
+
+const newArray1 = [...array];
+
+console.log(newArray1);
+
+//
+
+const q = {
+  one: 1,
+  two: 2
+};
+
+const newObj = {...q};
